5ec2ed202137850e04376e60e9cc4695f3ed91c9
Alexandra Nikiforova <sunny.alej@gmail.com>
2020-11-05 14:44:50 +0300

    modified code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,8 @@
 public class CreditPaymentService {
-    public int calculate(int credit_amount, int loan_terms, float rate) {
-        float monthly_percentage_rate = (float) (rate / 12);
-        int monthly_payment = (int) (credit_amount * ((monthly_percentage_rate * (Math.pow(1 + monthly_percentage_rate, loan_terms))) / (Math.pow(1 + monthly_percentage_rate, loan_terms) - 1)));
-        return monthly_payment;
+    public int calculate(int CreditAmount, int LoanTerms, float rate) {
+        float MonthlyRate = rate / 12;
+        float coefficient = (float) ((MonthlyRate * (Math.pow(1 + MonthlyRate, LoanTerms))) / (Math.pow(1 + MonthlyRate, LoanTerms) - 1));
+        int MonthlyPayment = (int) (CreditAmount * coefficient);
+        return MonthlyPayment;
     }
 }
