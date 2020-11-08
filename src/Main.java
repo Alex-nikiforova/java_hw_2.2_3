08db17ee73f0e40dfc3639003225c1eb766f852b
Alexandra Nikiforova <72600303+Alex-nikiforova@users.noreply.github.com>
2020-11-08 09:56:46 +0300

    modiffied variable names

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        int MonthlyPayment12 = service.calculate(1_000_000, 12, (float) 0.0999);
-        System.out.println(MonthlyPayment12);
-        int MonthlyPayment24 = service.calculate(1_000_000, 24, (float) 0.0999);
-        System.out.println(MonthlyPayment24);
-        int MonthlyPayment36 = service.calculate(1_000_000, 36, (float) 0.0999);
-        System.out.println(MonthlyPayment36);
+        int monthlyPayment12 = service.calculate(1_000_000, 12, (float) 0.0999);
+        System.out.println(monthlyPayment12);
+        int monthlyPayment24 = service.calculate(1_000_000, 24, (float) 0.0999);
+        System.out.println(monthlyPayment24);
+        int monthlyPayment36 = service.calculate(1_000_000, 36, (float) 0.0999);
+        System.out.println(monthlyPayment36);
     }
 }
